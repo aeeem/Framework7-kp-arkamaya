cdf2a9a0a0719b006b3479afcaca00320b30a098
aeeem <arifmaulanaa@gmail.com>
2018-05-28 15:02:19 +0700

    Changes to be committed: 	modified:   css/app.css 	modified:   css/index.css 	modified:   img/logo.png 	modified:   index.html 	modified:   js/app.js 	modified:   pages/Homepage.html
     Untracked files:
    	css/image/
    	css/style.css
    	img/bukanlogo.png
    	img/personalia.ai
    	img/personalia.png
    	js/const-var.js
    	js/script.js
    	js/sesuatu.js
    	js/user.js
    	pages/sesuatu.html
    	templet7/

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+import * as konstanta from './const-var.js';
+konstanta.storage.removeItem('session_id');
+konstanta.storage.clear();
 // Dom7
 var $$ = Dom7;
-
 // Framework7 App main instance
 var app = new Framework7({
   root: '#app', // App root element
@@ -8,6 +10,7 @@ var app = new Framework7({
   name: 'Apps', // App name
   theme: 'auto', // Automatic theme detection
   // App root data
+  
   data: function () {
     return {
       user: {
@@ -15,6 +18,7 @@ var app = new Framework7({
         lastName: 'Doe',
       },
       // Demo products for Catalog section
+      
       products: [
         {
           id: '1',
@@ -41,46 +45,49 @@ var app = new Framework7({
     },
   },
   // App routes
-  routes: routes,
+ 
+  
 });
+
+var sesuatu=app.request;
+
+
+
 
 
 // Init/Create views
 var homeView = app.views.create('#view-home', {
   url: '/'
-});
+}); 
 var settingsView = app.views.create('#view-settings', {
   url: '/settings/'
 });
 
-// Login Screen Demo
+
+
 $$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
-
-  if(!username)
-  {
-    app.dialog.alert('username tidak boleh kosong');
-    return;
+  var username= $$('#my-login-screen [name="username"]').val();
+  var password= $$('#my-login-screen [name="password"]').val();
+  var myObj={user_email:'',user_password:'',};
+  myObj.user_email=username;
+  myObj.user_password=password;
+  app.request.post(konstanta.api+'/Login',myObj , function (data) {
+    var dokumen = JSON.parse(data);
+    console.log(dokumen);
+    //cek apakah login berhasil ataau tidak, jika berhasil maka akan mengembalikan nilai true
+    if(!dokumen.status){
+      app.dialog.alert(dokumen.message);
+    }
+    /////////menampilkan pesan dari api dan menyimpan session kedalam local storage
+    else{
+      console.log(dokumen);
+    app.dialog.alert(dokumen.message);
+    dokumen.data.forEach(data => {
+      konstanta.storage.setItem('session_id',data.session_id); 
+    });
   }
-  if(!password){
-    app.dialog.alert('password tidak boleh kosong');
-  }
-if(username=="something"&&password=="something"){
-  app.dialog.alert('selamat datang '+username);
   
-  app.router.navigate("/homepage/");
-}
-});
-$$('#my-popup .regis-button').on('click', function () {
-  var username = $$('#my-popup [name="username"]').val();
-  var password = $$('#my-popup [name="password"]').val();
-  var email = $$('#my-popup [name="email"]').val();
-  var date = $$('#my-popup [name="tanggal"]').val();
- 
-    app.dialog.alert(date);
-   
-    return;
-  
+  });
+  });
 
-});
+
